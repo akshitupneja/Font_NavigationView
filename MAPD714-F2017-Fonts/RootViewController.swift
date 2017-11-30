460ae1839d6765db8c02131180aba658c3d3446e
Akshit Upneja <akki@Akshits-MacBook-Pro.local>
2017-11-29 20:43:29 -0500

    Modified Controller

---
+++ MAPD714-F2017-Fonts/RootViewController.swift
@@ -0,0 +1,93 @@
+//
+//  RootViewController.swift
+//  MAPD714-F2017-Fonts
+//
+//  Created by Akshit Upneja on 2017-11-29.
+//  Copyright © 2017 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITableViewController {
+
+    //private instance of variable which are availabe inside rood view controller
+    private var familyNames: [String]!
+    private var cellPointSize: CGFloat!
+    private var favoritesList: FavoritesList!
+    private static let familyCell = "FamilyName"
+    private static let favoritesCell = "Favorites"
+    
+    
+    // this event is triggered everytime the view is going to be rendered
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    
+    //this application event trigers when the first time application is loaded
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        familyNames = (UIFont.familyNames as [String]).sorted()
+
+        let preferredTableViewFont = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
+        cellPointSize = preferredTableViewFont.pointSize
+        favoritesList = FavoritesList.sharedFavoritesList
+        tableView.estimatedRowHeight = cellPointSize
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    
+    
+    //utility Methods
+    func fontForDisplay(atIndexPath indexPath: NSIndexPath) -> UIFont? {
+        if indexPath.section == 0 {
+            let familyName = familyNames[indexPath.row]
+            let fontName = UIFont.fontNames(forFamilyName: familyName).first
+            return fontName != nil ?
+                UIFont(name: fontName!, size: cellPointSize) : nil
+        } else {
+            return nil
+        }
+    }
+    
+    //DataSource Methods
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return favoritesList.favorites.isEmpty ? 1 : 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return section == 0 ? familyNames.count : 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "All Font Families" : "My Favorite Fonts"
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            // The font names list
+            let cell = tableView.dequeueReusableCell(withIdentifier: RootViewController.familyCell, for: indexPath)
+            cell.textLabel?.font = fontForDisplay(atIndexPath: indexPath as NSIndexPath)
+            cell.textLabel?.text = familyNames[indexPath.row]
+            cell.detailTextLabel?.text = familyNames[indexPath.row]
+            return cell
+        } else {
+            // The favorites list
+            
+            return tableView.dequeueReusableCell(withIdentifier: RootViewController.favoritesCell, for: indexPath)
+        }
+    }
+
+
+   
+}
