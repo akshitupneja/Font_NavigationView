d86f34239a193943b726cc9cbea26003c96a1933
Akshit Upneja <akki@Akshits-MacBook-Pro.local>
2017-11-29 21:31:26 -0500

    Added Favourite List view Controller

--- MAPD714-F2017-Fonts/RootViewController.swift
+++ MAPD714-F2017-Fonts/RootViewController.swift
@@ -88,6 +88,29 @@ class RootViewController: UITableViewController {
         }
     }
 
+    
+    //Navigate to Screen
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+        let indexPath = tableView.indexPath(for: sender as! UITableViewCell)!
+        let listVC = segue.destination as! FontListViewController
+        
+        if indexPath.section == 0 {
+            // Font names list
+            let familyName = familyNames[indexPath.row]
+            listVC.fontNames = (UIFont.fontNames(forFamilyName: familyName) as [String]).sorted()
+            listVC.navigationItem.title = familyName
+            listVC.showFavorites = false
+        } else {
+            // Favorites list
+            listVC.fontNames = favoritesList.favorites
+            listVC.navigationItem.title = "Favorites"
+            listVC.showFavorites = true
+        }
+    }
+    
 
    
 }
